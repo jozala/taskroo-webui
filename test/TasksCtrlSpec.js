fd67d485d0334345eca777f1e7cc902d6c837189
mariusz <mariusz@aetas.pl>
2014-07-28 23:24:17 +0100

    TasksCtrl tests corrected

--- test/TasksCtrlSpec.js
+++ test/TasksCtrlSpec.js
@@ -14,58 +14,65 @@ describe("TasksService responsible for managing tasks in the scope", function() 
 
     beforeEach(module("taskroo"));
 
+
+
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, TasksService) {
-        $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET('/ws/tasks').respond(testTasks);
+            $httpBackend = _$httpBackend_;
 
-        scope = $rootScope.$new();
-        tasksCtrl = $controller('TasksCtrl', {$scope: scope, TasksService: TasksService});
-    }));
+            scope = $rootScope.$new();
+            tasksCtrl = $controller('TasksCtrl', {$scope: scope, TasksService: TasksService});
+        })
+    );
 
-    it("should set tasks from WebService on creation of controller", function() {
+
+    it("should set tasks from WebService (unfinished) on creation of controller", function() {
+        $httpBackend.expectGET('/api/tasks?finished=false').respond(200, unfinishedTasks);
         $httpBackend.flush();
-        expect(scope.tasks).toEqualData(testTasks)
+        expect(scope.tasks).toEqualData(unfinishedTasks)
     });
 
     it("should set showUnfinishedTasks to true on creation of controller", function() {
         expect(scope.showUnfinished).toBe(true);
     });
 
-    it("should set finished tasks only in scope.tasks when showUnfinished changed to false", function() {
+    it("should set tasks from WebService(finished=true) in scope.tasks when showUnfinished changed to false", function() {
+        $httpBackend.expectGET('/api/tasks?finished=false').respond(200, unfinishedTasks);
         $httpBackend.flush();
+        $httpBackend.expectGET('/api/tasks?finished=true').respond(200, finishedTasks);
         scope.$apply(function() {
             scope.showUnfinished = false;
         });
-        var allTasksOnListAreFinished = scope.tasks.every(function(it) { return it.finished == true });
-        expect(scope.tasks.length).toBeGreaterThan(0);
-        expect(allTasksOnListAreFinished).toBe(true);
+        $httpBackend.flush();
+        expect(scope.tasks).toEqualData(scope.tasksFilter(finishedTasks))
     });
 
     it("should flatten list of finished task when showUnfinished is changed to false", function () {
-        mockRetrievedTasks(threeLevelFinishedTasks);
+        $httpBackend.expectGET('/api/tasks?finished=false').respond(200, unfinishedTasks);
         $httpBackend.flush();
-
+        $httpBackend.expectGET('/api/tasks?finished=true').respond(200, threeLevelFinishedTasks);
         scope.$apply(function() {
             scope.showUnfinished = false;
         });
+        $httpBackend.flush();
         expect(scope.tasks.length).toBe(3);
     });
 
     it("tasks should not have any subtasks when displaying finished tasks", function () {
-        mockRetrievedTasks(threeLevelFinishedTasks);
+        $httpBackend.expectGET('/api/tasks?finished=false').respond(200, unfinishedTasks);
         $httpBackend.flush();
-
+        $httpBackend.expectGET('/api/tasks?finished=true').respond(200, threeLevelFinishedTasks);
         scope.$apply(function() {
             scope.showUnfinished = false;
         });
+        $httpBackend.flush();
         var noneOfTheTasksHasSubtasks = scope.tasks.every(function(it) { return it.subtasks.length == 0 });
         expect(noneOfTheTasksHasSubtasks).toBeTruthy();
     });
 
     it("should replace task with subtasks with data returned by service when task is set as finished", function () {
-        mockRetrievedTasks(threeLevelUnfinishedTasks);
+        $httpBackend.expectGET('/api/tasks?finished=false').respond(200, threeLevelUnfinishedTasks);
         $httpBackend.flush();
-        $httpBackend.expectPUT('/ws/tasks/321').respond(threeLevelFinishedTasks[0]);
+        $httpBackend.expectPUT('/api/tasks/321').respond(threeLevelFinishedTasks[0]);
 
         scope.$apply(function() {
             scope.taskFinished(scope.tasks[0]);
@@ -76,15 +83,4 @@ describe("TasksService responsible for managing tasks in the scope", function() 
         expect(scope.tasks[0].subtasks[0].finished).toBe(true);
         expect(scope.tasks[0].subtasks[0].subtasks[0].finished).toBe(true);
     });
-
-    var mockRetrievedTasks = function(tasksList) {
-        inject(function (_$httpBackend_, $rootScope, $controller, TasksService) {
-
-            $httpBackend = _$httpBackend_;
-            $httpBackend.expectGET('/ws/tasks').respond(tasksList);
-
-            scope = $rootScope.$new();
-            tasksCtrl = $controller('TasksCtrl', {$scope: scope, TasksService: TasksService});
-        });
-    }
 });
